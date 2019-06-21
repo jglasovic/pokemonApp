145659e421d1c599e497ad1e4b053339e00775c9
Jure Glasovic <jglasovic@gmail.com>
2019-06-21 20:18:58 +0200

    fix apiservice for older sdk version

--- app/src/main/java/com/example/pokemonapp/rest/ApiGenerator.java
+++ app/src/main/java/com/example/pokemonapp/rest/ApiGenerator.java
@@ -1,11 +1,25 @@
 package com.example.pokemonapp.rest;
 
 import android.content.Context;
+import android.os.Build;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -36,6 +50,7 @@ public class ApiGenerator {
         httpClient.connectTimeout(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS);
         httpClient.readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS);
         httpClient.writeTimeout(WRITE_TIMEOUT, TimeUnit.MILLISECONDS);
+        httpClient = enableTls120nPreLollipop(httpClient);
 
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -53,5 +68,41 @@ public class ApiGenerator {
 
     public static ApiService buildService(Context context) {
         return service = createService();
+    }
+
+    private static OkHttpClient.Builder enableTls120nPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                        TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init((KeyStore) null);
+                TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+                if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                    throw new IllegalStateException("Unexpected default trust managers:"
+                            + Arrays.toString(trustManagers));
+                }
+                X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()), trustManager);
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Log.d(String.valueOf(123), "enableTls120nPreLollipop: failure!!! error:"+exc.toString());
+            }
+        }
+
+        return client;
     }
 }
